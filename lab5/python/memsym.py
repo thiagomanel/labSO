371131b0aa1c995e886b2cafbace8d8324321b7b
Thiago Manel <thiago.manel@gmail.com>
2017-03-13 16:45:51 -0300

    add TODOs

---
+++ lab5/python/memsym.py
@@ -0,0 +1,9 @@
+
+
+if __name__ = "__main__":
+    # read workload from input file
+    # read parameters: virtual size, physical size, page_replacement_alg
+    # setup simulation
+    # fire
+    # collect results
+    # write std out
