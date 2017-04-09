b0481ea7f6b4255f43155bb7e2eeb2750f4532f2
Rafael Perrella <rafael.clp@hotmail.com>
2017-04-09 01:14:53 +0000

    Add xeu_utils with parser

---
+++ lab2/xeu_utils/Command.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace xeu_utils {
+
+struct Command {
+  Command();
+
+  /**
+   * NOTE: this is useful for using in exec*.
+   * Returns the filename in the command (i.e. the first arg == argv[0]), or a
+   * pointer to an empty string if the command has no args.
+   */
+  const char* filename();
+
+  /**
+   * NOTE: this is useful for using in exec*.
+   * Returns a constant pointer to the args in the format required by some of
+   * exec variations (possibly the one you will choose to use).
+   */
+  char* const* argv();
+
+  /**
+   * Returns a list of all the args as a vector of strings.
+   */
+  const std::vector<std::string>& args();
+
+  /**
+   * Pushes a new arg to the arg list of the command.
+   */
+  void add_arg(const std::string& arg);
+
+  /**
+   * Escapes an arg and embeds it in double quotes.
+   */
+  static std::string escape_arg(const std::string& unescaped_arg);
+
+ private:
+  std::vector<char*> argv_; // stores pointers to the data (for use in exec*)
+  std::vector<std::string> args_; // stores the data
+};
+
+};
---
+++ lab2/xeu_utils/StreamParser.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ParsingState.h"
+
+#include <string>
+#include <iostream>
+
+namespace xeu_utils {
+
+struct StreamParser {
+  StreamParser(
+    const std::string& line_prefix="> ",
+    bool handle_errors=true
+  );
+
+  /**
+   * Parses the input until a final state is reached. Since a command may be
+   * split into multiple lines (by ending a line with \ or by using quotes),
+   * this method prints a line_prefix_ string to output for each extra new line.
+   */
+  ParsingState parse(
+    std::istream& input=std::cin,
+    std::ostream& output=std::cout
+  );
+
+  /**
+   * Similar to parse(istream&, ostream&), but copies the input string to
+   * unparsed_input, so you can use it later if you need.
+   */
+  ParsingState parse(
+    std::string& unparsed_input,
+    std::istream& input=std::cin,
+    std::ostream& output=std::cout
+  );
+
+ private:
+  std::string line_prefix_;
+  bool handle_errors_;
+};
+
+};
