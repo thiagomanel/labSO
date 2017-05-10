06da25adcb45efe2d61822bfab9299dca0e2b6a3
hukasu <lucas_fnf@hotmail.com>
2017-05-09 21:13:51 -0300

    Correcoes para Command

--- lab2/xeu_utils/Command.cpp
+++ lab2/xeu_utils/Command.cpp
@@ -10,6 +10,30 @@ Command::Command() {
   argv_.push_back(0);
 }
 
+Command::Command(const Command& other) {
+    argv_.push_back(0);
+    for (int i = 0; i < other.args_.size(); i++) {
+        add_arg(other.args_[i]);
+    }
+}
+
+
+Command& Command::operator=(const Command& other) {
+    args_.clear();
+    argv_.push_back(0);
+    for (int i = 0; i < other.args_.size(); i++) {
+        add_arg(other.args_[i]);
+    }
+    return *this;
+}
+
+Command::~Command() {
+    for (int i = 0; i < argv_.size()-1; i++) {
+        delete[] argv_[i];
+    }
+    argv_.clear();
+}
+
 const char* Command::filename() {
   return argv_[0];
 }
@@ -24,7 +48,12 @@ const std::vector<std::string>& Command::args() {
 
 void Command::add_arg(const std::string& arg) {
   args_.push_back(arg);
-  argv_.back() = const_cast<char*>(args_.back().c_str());
+  
+  char* n = new char[arg.length()+1];
+  memcpy(n, arg.c_str(), arg.length());
+  n[arg.length()] = 0;
+  
+  argv_.back() = n;
   argv_.push_back(0);
 }
 
