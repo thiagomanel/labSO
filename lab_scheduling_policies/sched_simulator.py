1105817d642d57f663f9f5c1deace5e7902abf47
Thiago Manel <thiago.manel@gmail.com>
2018-05-22 14:37:19 -0300

    skeleton to sched simulator

---
+++ lab_scheduling_policies/sched_simulator.py
@@ -0,0 +1,17 @@
+
+
+def run_simulation():
+    pass
+
+
+#main
+
+#read the args
+    #read workload file in the standard directory
+    #run the simulator config
+        #interrupt interval
+        #tick time
+#parse the workload file
+#pass the arg to run_simulation
+#get the return values from run_simulation
+#generate the raw output to the standard output
