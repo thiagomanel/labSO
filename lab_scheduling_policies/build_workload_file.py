fe6d55938faf4136a82bfb718784482538504697
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2018-05-23 09:19:28 -0300

    Workload file parser

---
+++ lab_scheduling_policies/build_workload_file.py
@@ -0,0 +1,25 @@
+from random import randint
+
+def build_process_file():
+    f = open('workload_file.ffd', 'w')
+    proc_list = []
+    timestamp_c = 1
+    pid_c = 1
+
+    for i in xrange(30):
+        priority = randint(0, 20)
+        service_time = randint(40, 400)
+        proc_list.append(
+            str(timestamp_c) + ' ' +
+            str(pid_c) + ' ' +
+            str(priority) + ' ' +
+            str(service_time) + '\n'
+        )
+        timestamp_c += randint(1, 50)
+        pid_c += 1
+    
+    f.writelines(proc_list)
+    f.close()
+
+if __name__ == '__main__':
+    build_process_file()
--- lab_scheduling_policies/sched_simulator.py
+++ lab_scheduling_policies/sched_simulator.py
@@ -1,10 +1,51 @@
+from sys import stderr
 
+class Process(object):
+    def __init__(self, timestamp, pid, priority, service_t):
+        self.timestamp = timestamp
+        self.pid = pid
+        self.priority = priority
+        self.service_t = service_t
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_pid(self):
+        return self.pid
+
+    def get_priority(self):
+        return self.priority
+
+    def get_service_t(self):
+        return self.service_t
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class WorkloadParser(object):
+    def parse(self, filename):
+        file_lines = []
+        try:
+            with open(filename, 'r') as wk_load_file:
+                file_lines = wk_load_file.readlines()
+        except IOError:
+            stderr.write('Unable to read workload file!\n')
+        proc_list = []
+        for line in file_lines:
+            timestamp, pid, priority, service_t = map(int, line.split())
+            proc_list.append(Process(timestamp, pid, priority, service_t))
+        return proc_list
+
+ordered_process_list = []
 
 def run_simulation():
     pass
 
 
-#main
+if __name__ == '__main__':
+    wlp = WorkloadParser()
+    ordered_process_list = wlp.parse('workload_file.ffd')
 
 #read the args
     #read workload file in the standard directory
