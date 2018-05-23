fd4663188aab13836e2b3dad69bcc9f7c1f4047e
Thiago Manel <thiago.manel@gmail.com>
2018-05-23 15:38:07 -0300

    add stubs

--- lab_scheduling_policies/sched_simulator.py
+++ lab_scheduling_policies/sched_simulator.py
@@ -2,6 +2,7 @@ from sys import stderr
 
 class Process(object):
     def __init__(self, timestamp, pid, priority, service_t):
+        #FIXME: add usage time to be updated by the engine
         self.timestamp = timestamp
         self.pid = pid
         self.priority = priority
@@ -37,11 +38,34 @@ class WorkloadParser(object):
             proc_list.append(Process(timestamp, pid, priority, service_t))
         return proc_list
 
+def now():
+    #to be implemented - engine method
+    """Return the current logical timestamp"""
+    pass
+
+def alloc_proc(process):
+    #plugin method
+    """Update the data structures to recognize a new process was created"""
+    pass
+
+def schedule(out_process):
+    #plugin method
+    """Return the next process to run in the cpu.
+
+    out_process -- the process that just left the cpu, or None in case there
+                was no process running. The engine is responsible for updating
+                the usage time.
+    """
+    pass
+
 ordered_process_list = []
 
 def run_simulation():
-    pass
 
+    #get the next event
+    #check its type
+    #call the handler associated witht the type
+    pass
 
 if __name__ == '__main__':
     wlp = WorkloadParser()
