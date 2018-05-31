d8e034c7762acda13188eb48f9c08a94a21bbec0
Matteus Silva <matteus.silva@ccc.ufcg.edu.br>
2018-05-31 20:49:20 -0300

    Save plot on disk.

--- lab_scheduling_policies/generate_plot.R
+++ lab_scheduling_policies/generate_plot.R
@@ -4,6 +4,6 @@ library(timelineS)
 # Loads the input file.
 raw_data <- read.table("timeline-output.ffd", header = T, sep = " ")
 
+png(filename = "timeline_plot.png", width = 1000, height = 600, units = "px",res = 120)
 timelineG(df = raw_data, names = "process", phase = "service", start = "start_t", end = "end_t")
-
-
+dev.off()
