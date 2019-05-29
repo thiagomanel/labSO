28294e262368d7448b12c309abf9a252222d2bcd
joseims <jose.schmid@ccc.ufcg.edu.br>
2019-05-29 08:59:03 -0300

    correcting asked stuff

--- io_scheduling/disk.py
+++ io_scheduling/disk.py
@@ -1,7 +1,7 @@
 class Disk:
-	def __init__(self,track_size):
-		self.track_size=track_size
+	def __init__(self, track_size):
+		self.track_size = track_size
 		self.current_position = 0
 
-	def distance(self,new_position):
-		return (self.current_position - new_position)%self.track_size
+	def distance(self, position):
+		return (self.current_position - position) % self.track_size
--- io_scheduling/scheduler.py
+++ io_scheduling/scheduler.py
@@ -2,10 +2,13 @@ import random as r
 
 class Scheduler:
 	def schedule(queue):
-		return 0
+		if (len(queue) > 0)
+			return queue.pop()
 
 
 
 class Random_scheduler(Scheduler):
 	def schedule(queue):
-		return r.randint(0,len(queue))
+		if (len(queue) > 0)
+			selected = r.randint(0,len(queue))
+			return queue.pop(selected)
